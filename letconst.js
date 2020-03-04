aa338e6a221b5bd6f5cc17e2c024f5f0d6edf80e
anower93 <anowerhossin1993@gmail.com>
2020-03-04 23:15:07 +0600

    JavaScritp ES6

---
+++ letconst.js
@@ -0,0 +1,26 @@
+const bd = "I love you";
+
+
+console.log(bd);
+
+const nams = [100, 522, 999];
+
+nams.shift();
+nams.unshift("Hi");
+nams.pop();
+nams.push("Hello");
+
+// const declare korle man change or man dewa jabe
+//Ei name ar kono array declare kora jabe na
+
+//Exmaple: const nams = ["Something", "is", "better"];
+
+console.log(nams);
+
+// Const use kore abar Object banano jay. Jemon-
+
+const apsit = {name:"Anower", phone:1722142400};
+
+console.log(apsit);
+
+
