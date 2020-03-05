9e9f7cbaa746f8d558b378f217af742d6ded4684
anower93 <anowerhossin1993@gmail.com>
2020-03-05 23:23:13 +0600

    Template

---
+++ template.js
@@ -0,0 +1,14 @@
+//template diye khub sohoje string related kaj kora jay.
+//String er maje variable add kora jay `` karet er maddome 
+// Multi-line add kora jay
+
+const ap = 2020;
+
+
+const abid = `I love you 
+
+I like you 
+
+${ap} is my challanging year.
+
+`
