e1b1e2895014fe750e71fe282432e64f8b1820e5
anower93 <anowerhossin1993@gmail.com>
2020-03-05 23:05:20 +0600

    default value

---
+++ defaultparameter.js
@@ -0,0 +1,14 @@
+
+
+// Jodi amra default kon man parameter a na dai
+//tahole NaN lekhat show korbe. 
+//ejonno dafualt a ekta kisu dite hobe
+
+
+function add(num, num2=0){
+    return num+num2;
+}
+
+const total = add(11,22);
+
+console.log(total);
