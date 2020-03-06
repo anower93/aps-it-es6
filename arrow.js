25d4d8ceb4d12da503274f4e4bd10f3c0b3b7907
anower93 <anowerhossin1993@gmail.com>
2020-03-06 12:49:10 +0600

    Math Update

--- arrow.js
+++ arrow.js
@@ -1,11 +1,37 @@
 //amra sdharonoto evabe function use kori---
 
-function ap(num){
-    return num;
+// function ap(num){
+//     return num*5;
+// }
+
+
+// const ap = function something(num){
+//     return num*3;
+// }
+
+    //This is single parameter arrow function
+// const ap = num => num*2;
+
+
+    //More than one parameter arrow function
+// const add = (a, b) => a+b;
+
+    //Without parameter for arrow function
+
+// const result = () => "I love programming!";
+
+
+// Huge function with arrow function
+
+const ap = (x, y) => {
+    const add = x + y;
+    const minus = x - y;
+    const multi = add * minus;
+    return multi;
 }
 
-const pa = ap("100% Okay ase!");
+const result = ap(7, 5);
 
-console.log(pa);
+console.log( "Result is: " + result);
 
 
