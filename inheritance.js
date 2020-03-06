9d0741a918c6a07b95403ef787d19a699b6946f8
anower93 <anowerhossin1993@gmail.com>
2020-03-06 20:04:10 +0600

    Method Update

--- inheritance.js
+++ inheritance.js
@@ -8,9 +8,22 @@ class Father {
 
 class Child extends Father{
     constructor(name, mother){
+        //JavaScript a Child class a 
+        //super() method call korte hobe
+
         super();
+
         this.name = name;
         this.mother = mother;
+    }
+
+
+    getFunction(){
+        //Ei method thiye Parant class Or Child class er 
+        // jekono property access kora jabe.
+
+        return this.fatherName + "\n" + this.mother;
+
     }
 
 }
@@ -20,3 +33,4 @@ const putuputu = new Child("Tahmid", "Priyoti");
 const akupaku = new Child("Tanshir", "Priyoti");
 
 console.log(putuputu, akupaku);
+console.log(putuputu.getFunction());
